47f11a4cc97a93e5d95d5d8515bf9683ac8319e0
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-06 19:34:58 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "zahoor"
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+
+ let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+//  "33" => 33
+// "332aa" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
