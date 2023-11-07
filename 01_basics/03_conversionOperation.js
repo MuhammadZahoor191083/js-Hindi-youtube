3c5b323271945de49434ebb33653c569fb713a08
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-07 14:04:38 +0000

    datatype summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,7 +4,7 @@ let score = "zahoor"
 //  console.log(typeof (score));
 
 
- let valueInNumber = Number(score)
+//  let valueInNumber = Number(score)
 //  console.log(typeof valueInNumber);
 //  console.log(valueInNumber);
 
@@ -13,6 +13,48 @@ let score = "zahoor"
 // "332aa" =>NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+// *******Operation**********//
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 + 3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "zahoor"
+// let str2 = " hello"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" + 2 + 2);
+
+
+// console.log((3 + 3) * 5 % 3); 
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// read prefix and postfix in mdn
---
+++ datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive Data Types
+
+// 7 types : String, Number, Bolean, Null, undefined, Symbol(for unique value), BigInt(for large value)
+
+const score = 100
+const scoreValue = 100.5
+const isLoggedIn = false
+const outSidetemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+// const bigNumber = 73247012370497953972390n
+
+
+
+// Reference Type(Non primitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["zahoor", "Afridi", "Khalil", "Ali"];
+let myObj = {
+    name: "zahoor",
+    age: 22,
+}
+
+const myFunction =function() {
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
