b49a7a7842943cbbba59bfa319d5765d0071e24f
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-08 04:41:47 +0000

    strings

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -22,4 +22,4 @@
 
 // ===
 
-// console.log("2" === 2);
+// console.log("2" === 2); // strict check
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "hitesh"
+const repoCount = 50
+// console.log(name + repoCount + "Value"); outdate
+
+//  use backticks
+ console.log(`hello my name is ${name}and my repo count is ${repoCount}`);
+
+
+
+ const gameName = newString('hiteshhhc')
+
+ console.log(gameName[0]);
+
+ console.log(gameName.__proto__);
+
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+
+ const newString = gameName.substring(0,5)
+ console.log(newString);
+
+
+ const anorherString = gameName.slice(-8, 4)
+ console.log(anorherString);
+
+
+ const newStringOne = "  hitesh   "
+
+ console.log(newStringOne);
+ console.log(newStringOne.trim);
+
+
+
+ const url = " https://hitesh.com/hiteshc%20choudary"
+
+lo
