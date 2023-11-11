d58985f6f7ac8da689ecf91eab98f3df6d5f1290
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-11 01:09:14 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,7 +6,7 @@ const tinderUser = new Object()
 tinderUser.id = "212abs"
 tinderUser.name = "zahoor"
 tinderUser.isLoggedIn = false
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
 // const regularUser = {
@@ -33,26 +33,54 @@ const obj3 = {...obj1, ...obj2, ...obj4}
 
 // console.log(obj3);
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
 
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
 
-    },
-]
+//     },
+// ]
 
-users[1].email
+// users[1].email
 
 // console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+
+}
+// course.courseInstructor
+const {courseInstructor:instructor} = course
+
+console.log(instructor);
+
+// {
+//     name: "hitesh",
+//     coursename: "js in hindi",
+//     course: "free"
+// }
+
+
+
+[
+    {},
+    {},
+    {},
+]
+
