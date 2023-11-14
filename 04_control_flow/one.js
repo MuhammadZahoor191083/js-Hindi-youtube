8ee15ff7f2e63de7ebac4b820aabeb35fdee3fa9
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-14 19:28:54 +0000

    && and || condition in If

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -24,22 +24,42 @@
 
 // console.log(`User power: ${power}`);
 
-const balance = 1000
-// if(balance > 500) console.log("test"), console.log("test2"); // dont use it.
+// const balance = 1000
+// // if(balance > 500) console.log("test"), console.log("test2"); // dont use it.
 
-if (balance < 500) {
-     console.log("less than 500");
+// if (balance < 500) {
+//      console.log("less than 500");
 
-} else if (balance < 750) {
-    console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 500");
 
-} else if ( balance < 900) {
-    console.log("less than 750");
+// } else if ( balance < 900) {
+//     console.log("less than 750");
 
-} else if(balance < 900){
-    console.log("less than 750");
+// } else if(balance < 900){
+//     console.log("less than 750");
 
-} else {
-    console.log("less than 1200");
+// } else {
+//     console.log("less than 1200");
 
+// }
+
+
+
+
+const userLoggedIn = true
+const debitCard = true
+
+const loggedInFromGoogle = false
+
+const loggedInFromEmail = true
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
 }
+
+// if (userLoggedIn && debitCard ) {
+//     console.log("Allow to buy course");
+    
+// }
