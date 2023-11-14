b81071bf15561053a12a992de8b892f4e5dba171
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-14 13:51:11 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+//  If
+
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature === 40 ) {
+//     console.log("less than 50"); //
+// } else {
+//     console.log("temperatire is greater than 50");
+// }
+
+// console.log("Execute");
+
+// <,>, <=, >=, ==, !=, ===(also check the type), !==
+
+
+// const score = 200
+
+// if (score > 100) {
+//    let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+// if(balance > 500) console.log("test"), console.log("test2"); // dont use it.
+
+if (balance < 500) {
+     console.log("less than 500");
+
+} else if (balance < 750) {
+    console.log("less than 500");
+
+} else if ( balance < 900) {
+    console.log("less than 750");
+
+} else if(balance < 900){
+    console.log("less than 750");
+
+} else {
+    console.log("less than 1200");
+
+}
