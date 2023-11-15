3960b11aad5dcd405ff78c5c123fb81193ffd4a3
Muhammad Zahoor <93613988+MuhammadZahoor191083@users.noreply.github.com>
2023-11-15 07:06:35 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,29 @@
+//  for 
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+    
+}
+
+// console.log(element);  
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and innner loop ${i}`);
+        
+        // console.log(i + '*' + j +' = ' + i*j);
+    }
+    
+}
+let myArray = ["flash", "ok", "superman"]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+    
+}
